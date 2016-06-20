36971503f98352e0d764cecf3a2aced142a6c2c6
root <root@spacewalk.ruzik.net>
2016-06-20 19:38:35 +0300

    Add Collection for Vagon Type

--- app/models/vagon.rb
+++ app/models/vagon.rb
@@ -1,4 +1,13 @@
 class Vagon < ActiveRecord::Base
   belongs_to :train
-  validates :vagon_type, presence: :true
+  validates :number, :top_seats, :bottom_seats, presence: :true
+
+  before_validation :set_number
+
+  private
+
+  def set_number
+    self.number = Random.rand(0...10)
+  end
+
 end
