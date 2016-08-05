cf01f1bc5914864df82dce086a467270801b7f34
root <root@debian>
2016-08-05 14:05:21 -0500

    Added Redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:myname, :myusurname, :email)
+    end
+  end
   
-  def after_sign_in_path_for(user)
-    root_path if current_user.admin?
+  def after_sign_in_path_for(resource)
+      if current_user.admin?
+        root_path
+      else
+        new_search_path
+      end
   end
 
 end
